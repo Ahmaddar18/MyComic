2688990c62029699c3c356301a9493a6f8d7bd5f
Ahmaddar18 <kooldc@kools-mac.local>
2018-12-06 00:52:35 -0800

    Initial Commit

---
+++ MyComic/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MyComic
+//
+//  Created by kool dc on 12/6/18.
+//  Copyright © 2018 Ahmad. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ MyComicTests/MyComicTests.swift
@@ -0,0 +1,34 @@
+//
+//  MyComicTests.swift
+//  MyComicTests
+//
+//  Created by kool dc on 12/6/18.
+//  Copyright © 2018 Ahmad. All rights reserved.
+//
+
+import XCTest
+@testable import MyComic
+
+class MyComicTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
